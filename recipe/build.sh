363bc8dbd4203730b1df0b5d07d8b79eb3a8307d
Sylvain Corlay <sylvain.corlay@gmail.com>
2018-12-19 20:35:52 +0100

    Fixup libdir

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,7 @@
 cmake -DCMAKE_BUILD_TYPE=Release     \
       -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DDISABLE_ARCH_NATIVE=ON       \
+      -DCMAKE_INSTALL_LIBDIR=lib    \
       $SRC_DIR
 
 make install
